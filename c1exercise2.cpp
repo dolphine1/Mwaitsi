a8e61d84c2b085dcdbda416b67a42797cc616a5e
Dolphine <254talidolphine@gmail.com>
2025-12-28 01:32:12 +0300

    exercise 2

---
+++ c1exercise2.cpp
@@ -0,0 +1,25 @@
+//Change the "Hello, World!" example above to display another line. If Spock were doing this exercise, he might add to it so that it would display:
+
+//  output: Hello, World!
+//          Live long and prosper.
+//code:
+// #include<iostream>
+// using name space std;
+// intmain()
+// {
+//  cout<<"Hello, World!"<<endl;
+//  cout<<"Live long and prosper."<<endl;
+//  cin.get();
+//  return 0;
+// } 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello,World!" << endl;
+    cout << "Live Long and prosper" <<endl;
+    cin.get();
+    return 0;
+}
