6638a51bac5073d5345265587c0708d132a77ade
Dolphine <254talidolphine@gmail.com>
2025-12-28 00:53:10 +0300

    testing the difference between my first program and saylor's

---
+++ new1.cpp
@@ -0,0 +1,17 @@
+//testing the difference between my first program and saylor's
+//one major difference
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello stranger" << endl;
+    cin.get();  //the one major difference i know nothing about
+    return 0;
+}
+
+// observation after running code
+// my ouput is printed as usual but theres a new line
+//my guess is that since character in is for new inputs it waits for one
+//then when i press enter theres no input here so it returns 0
